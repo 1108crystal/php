f9501415c5aa8b0d6b11d91b21d228c677b0b718
crystal <1108crystal.gmail.com>
2022-11-07 16:30:17 +0800

    add 活動日期

--- calendar.php
+++ calendar.php
@@ -1,3 +1,8 @@
+<!-- php不是有內建function嗎
+https://www.startutorial.com/articles/view/how-to-build-a-web-calendar-in-php
+
+曆法從來就不是簡單的事情, 這個不知道有沒有幫助: https://gist.github.com/eagleon/1702129 -->
+
 <?php
 $cal = []; //指定月份的日期表
 
@@ -53,9 +58,16 @@ for ($i = 1; $i <= $monthDays; $i++) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>萬年曆-作業 (23號)</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css"
+        integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="/css/style.css">
 </head>
@@ -63,7 +75,41 @@ for ($i = 1; $i <= $monthDays; $i++) {
 <body class="main">
     <!-- header -->
 
-    <a href='index.php'>回首頁</a><br>
+    <!-- <a href='index.php'>回首頁</a><br> -->
+        <!-- A grey horizontal navbar that becomes vertical on small screens -->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><i class="fa-brands fa-twitter"></i>萬年曆</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="index.php">回首頁</a>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="calendar.php">簡易萬年曆</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">萬年曆</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button"
+                            data-bs-toggle="dropdown">課程</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="">課程1</a></li>
+                            <li><a class="dropdown-item" href="">課程2</a></li>
+                            <li><a class="dropdown-item" href="">課程3</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <hr>
+
 
 
     <!-- 中間 left-->
@@ -74,12 +120,12 @@ for ($i = 1; $i <= $monthDays; $i++) {
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-5" >
 
-            <img  src="./images/kimono/<?=$imgfiles;?>" class="rounded" alt="Cinque Terre">
+            <img width="640px" height="800px" src="./images/kimono/<?=$imgfiles;?>" class="rounded" alt="Cinque Terre">
                 <!-- <img src="./images/kimono/01.jpg" class="img-thumbnail" alt="Cinque Terre"> -->
             </div>
-            <div class="col-sm-9">
+            <div class="col-sm-7">
 
                 <table class="table">
                     <tr>
